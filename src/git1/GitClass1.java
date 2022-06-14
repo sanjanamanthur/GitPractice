1856c5c0d6743a662edbcc9473ca2469ddddee25
sanjana <manthursanjanareddy@gmail.com>
2022-06-14 21:34:21 +0530

    first modification

--- src/git1/GitClass1.java
+++ src/git1/GitClass1.java
@@ -5,6 +5,9 @@ public class GitClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("git new project");
+		System.out.println("first modification");
+		
 	}
 
 }
