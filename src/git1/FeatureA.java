941928cbddc37f66f56166070fae39936b38168e
sanjana <manthursanjanareddy@gmail.com>
2022-06-14 21:43:28 +0530

    Some modifications in FeatureA

--- src/git1/FeatureA.java
+++ src/git1/FeatureA.java
@@ -5,6 +5,7 @@ public class FeatureA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("FeatureA");
+System.out.println("FeatureA modifications");
 	}
 
 }
